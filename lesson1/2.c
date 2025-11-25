fe4f04adc375211c3c4f370c3c0d3d29f7dfe5fb
Tair <gidrolaz.tr@gmail.com>
2025-11-25 16:34:30 +0700

    Complete 2.c and init 3.c

--- lesson1/2.c
+++ lesson1/2.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
-//Вывести двоичное представление целого положительного числа, используя битовые операции (число вводится с клавиатуры).
+void printBinary(unsigned int number)
+{
+    if (number == 0) 
+    {
+        printf("0\n");
+        return;
+    }
+
+    number = ~number+1;
+
+    int temp = 1;
+    while (temp <= number/2) 
+    {
+        temp <<= 1; 
+    }
+
+    while (temp > 0) 
+    {
+        if (number & temp) 
+        {
+            printf("1");
+        } 
+        else 
+        {
+            printf("0");
+        }
+        temp >>= 1; 
+    }
+
+    printf("\n");
+}
+
+//Вывести двоичное представление целого отрицательного числа, используя битовые операции (число вводится с клавиатуры).
 int main() 
 {
-    
+    int number;
+    scanf("%d", &number);
+    printBinary(number);
+
+    return 0;
 }
