2d07af4083afc2874fcccec6a2377dc66dac55d2
Tair <gidrolaz.tr@gmail.com>
2025-11-25 16:11:05 +0700

    Finish first task and init second

--- lesson1/1.c
+++ lesson1/1.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 
-
-int main()
+void printBinary(unsigned int number)
 {
-    printf("test");
+    if (number == 0) 
+    {
+        printf("0\n");
+        return;
+    }
+
+    unsigned int temp = 1;
+    while (temp <= number/2) 
+    {
+        temp <<= 1; 
+    }
+
+    while (temp > 0) 
+    {
+        if (number & temp) 
+        {
+            printf("1");
+        } 
+        else 
+        {
+            printf("0");
+        }
+        temp >>= 1; 
+    }
+
+    printf("\n");
+}
+
+//Вывести двоичное представление целого положительного числа, используя битовые операции (число вводится с клавиатуры).
+int main() 
+{
+    unsigned int number;
+    scanf("%d", &number);
+    printBinary(number);
+
+    return 0;
 }
