8923241562933a88425e7056e0bf70aeb0435b5f
Tair <gidrolaz.tr@gmail.com>
2025-11-27 09:57:27 +0700

    Ex1 completed

--- lesson2/1.c
+++ lesson2/1.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
 #define N 3
 
+// Вычисляет количество цифр в числе
+int countDigits(int num) 
+{
+    if (num == 0) return 1;
+    
+    int count = 0;
+    int n = num;
+    while (n > 0) 
+    {
+        count++;
+        n /= 10;
+    }
+    
+    return count;
+}
 
-// Вывести квадратную матрицу по заданному N
+// Выводит квадратную матрицу с выравниванием по максимальному элементу массива
+void printArray(int arr[][N])
+{
+    int maxLength = countDigits(N*N);
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%*d ", maxLength, arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Задание: Вывести квадратную матрицу по заданному N
+
 int main() 
 {
-    
+    int array[N][N];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            array[i][j] = j + N * i + 1;
+        }
+    }
+    printArray(array);
+    return 0;
 }
