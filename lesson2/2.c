81031246caa265985f1a1bb746220b95719ee861
Tair <gidrolaz.tr@gmail.com>
2025-11-27 10:04:25 +0700

    Complete ex2

--- lesson2/2.c
+++ lesson2/2.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
-#define N 3
+
+#define N 5
 
 
 // Вывести заданный массив размером N в обратном порядке.
 int main() 
 {
-
+    int array[N];
+    
+    // Ввод массива
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    
+    // Вывод в обратном порядке
+    for (int i = N - 1; i >= 0; i--)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    return 0;
 }
