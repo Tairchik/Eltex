dc0cb9bc295b816633a24da3cff23c8bfd649f36
Tair <gidrolaz.tr@gmail.com>
2025-11-28 19:27:53 +0700

    Complete 3

--- lesson2/3.c
+++ lesson2/3.c
@@ -2,8 +2,33 @@
 #define N 3
 
 
-// Заполнить верхний треугольник матрицы а нижний 0.
+// Задание: заполнить верхний треугольник матрицы 1, а нижний 0.
 int main() 
 {
+    int array[N][N];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (j < N - i - 1)
+            {
+                array[i][j] = 0; 
+            }
+            else
+            {
+                array[i][j] = 1;   
+            }
+        }
+    }
 
+     // Ввод массива
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%d ", array[i][j]);
+
+        }
+        printf("\n");
+    }
 }
