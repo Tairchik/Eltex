89f5305dbbb6c2a109e0d26ba24f8c440bc4945d
Tair <gidrolaz.tr@gmail.com>
2026-03-30 14:34:19 +0700

    init ex3 in lesson 12

---
+++ lesson12/ex3/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "shell.h"
+
+/*
+Переписать командный интерпретатор из задания по процессам так,
+чтобы он поддерживал конструкции вида:
+$ ls -la | grep a
+*/
+
+
+int main()
+{
+    sh_start();
+    exit(EXIT_SUCCESS);
+}
