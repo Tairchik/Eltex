26143f15601e4028cc75bdd49ed9385d438a72ef
Tair <gidrolaz.tr@gmail.com>
2026-04-21 10:17:48 +0700

    Rename dir

--- lesson16/part 2/Parallel_Server/server.h
+++
@@ -1,5 +0,0 @@
-#define BUFF_SIZE 64
-#define PORTLISTEN 5432
-
-int service_server(int port, int clientfd_listen);
-int listen_server();
--- lesson16/part 2/Pool_Parallel_Server/server.h
+++
@@ -1,7 +0,0 @@
-#include <semaphore.h>
-
-#define BUFF_SIZE 64
-#define PORTLISTEN 12345
-
-int service_server(int port, sem_t *sem);
-int listen_server();
